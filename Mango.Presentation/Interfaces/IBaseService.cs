0937eedc82a7dbfc467fea1fac9262d1cc6051df
masanori-iha <masanori.iha@outlook.com>
2025-12-13 22:46:10 -0300

    Add project files.

---
+++ Mango.Presentation/Interfaces/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Presentation.Models.Dtos;
+
+namespace Mango.Presentation.Interfaces
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
---
+++ Mango.Presentation/Models/Dtos/RequestDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Mango.Presentation.Models.Dtos
+{
+    public class RequestDto
+    {
+        public RequestType ApiType { get; set; } = RequestType.GET;
+        public string Url { get; set; }
+        public Object Data { get; set; }
+        public string AccessToken { get; set; }
+
+    }
+
+    public enum RequestType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}
